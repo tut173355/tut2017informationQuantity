33b2e0acc2b9ff9cffd784b4e529a957e3d5f14f
FUJII TAKUYA <t173355@edu.tut.ac.jp>
2017-12-19 16:36:09 +0900

    saishin

--- s4/b173355/TestCase.java
+++ s4/b173355/TestCase.java
@@ -3,15 +3,16 @@ import java.lang.*;
 import s4.specification.*;
 
 /*
-interface FrequencerInterface {     // This interface provides the design for frequency counter.
-    void setTarget(byte[]  target); // set the data to search.
-    void setSpace(byte[]  space);  // set the data to be searched target from.
-    int frequency(); //It return -1, when TARGET is not set or TARGET's length is zero
-                    //Otherwise, it return 0, when SPACE is not set or Space's length is zero
-                    //Otherwise, get the frequency of TAGET in SPACE
+ 
+interface FrequencerInterface {     //このインタフェースは、周波数カウンタの設計を提供します。    
+    void setTarget(byte[]  target); //検索するデータを設定します。
+    void setSpace(byte[]  space);  //検索対象となるデータを設定します。
+    int frequency();    // TARGETが設定されていないか、TARGETの長さがゼロの場合は-1を返します
+                        //それ以外の場合は、SPACEが設定されていないか、スペースの長さがゼロの場合は0を返します。
+                        //それ以外の場合は、SPACEのTAGETの頻度を取得します。
     int subByteFrequency(int start, int end);
-    // get the frequency of subByte of taget, i.e target[start], taget[start+1], ... , target[end-1].
-    // For the incorrect value of START or END, the behavior is undefined.
+    // target [start]、target [start + 1]、...、target [end-1]のように、targetのsubByteの頻度を取得します。
+    // STARTまたはENDの値が正しくない場合、動作は未定義です。
 }
 */
 
@@ -31,8 +32,8 @@ public interface InformationEstimatorInterface{
 
 public class TestCase {
     public static void main(String[] args) {
-	try {
-	    FrequencerInterface  myObject;
+        try {
+            FrequencerInterface  myObject;
 	    int freq;
 	    System.out.println("checking s4.b173355.Frequencer");
 	    myObject = new s4.b173355.Frequencer();
@@ -45,6 +46,28 @@ public class TestCase {
 	catch(Exception e) {
 	    System.out.println("Exception occurred: STOP");
 	}
+    try {
+        FrequencerInterface  myObject;
+        int freq;
+        int num;
+        System.out.println("checking s4.b173355.Frequencer");
+        myObject = new s4.b173355.Frequencer();
+        myObject.setSpace("Hi Ho Hi Ho".getBytes());
+        myObject.setTarget("H".getBytes());
+        freq = myObject.frequency();
+        /*System.out.print("\"H\" in \"Hi Ho Hi Ho\" appears "+freq+" times. ");
+         for(int i=0; i<4 ; i++){
+            for(int j=0; j<4 ; j++){
+                System.out.print(i+" "+j+ ":" );
+                nummyObject.subByteFrequency(i,j));
+            }
+         }*/
+        num = myObject.subByteFrequency(0,4);
+        if(-1 == num) { System.out.println("OK"); } else {System.out.println("WRONG"); }
+    }
+    catch(Exception e) {
+        System.out.println("Exception occurred: STOP");
+    }
 
 	try {
 	    InformationEstimatorInterface myObject;
@@ -67,8 +90,7 @@ public class TestCase {
 	}
 	catch(Exception e) {
 	    System.out.println("Exception occurred: STOP");
-	}
-
     }
-}	    
+    }
+}
 	    
