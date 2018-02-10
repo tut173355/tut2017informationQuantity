80115cf04d8cd57b5f0a4ef4f9e0323c9c0e164a
FUJII TAKUYA <t173355@edu.tut.ac.jp>
2018-02-10 21:49:11 +0900

    2/10

--- s4/b173355/Frequencer.java
+++ s4/b173355/Frequencer.java
@@ -43,13 +43,29 @@ public class Frequencer implements FrequencerInterface{
     
  	//public void setSpace(byte []space) { mySpace = space; }
    	public void setSpace(byte []space) { //mySpace = space;
-        mySpace = space; if(mySpace.length>0)  spaceReady = true;
+        mySpace = space; 
+        if(mySpace.length>0)  spaceReady = true;
         suffixArray = new int[space.length];
         // put all suffixes in suffixArray. Each suffix is expressed by one interger.
         for(int i = 0; i< space.length; i++) {
             suffixArray[i] = i;
         }
-        for(int i=0;i<space.length;i++){
+        /*ソート*/
+        for (int i = 0; i < space.length; i++) {
+			int min = i;
+			for (int j = i + 1; j < space.length; j++){
+				//if (array[min] > array[j]){
+				if (suffixCompare(min, j) != -1){
+					min = j;
+				}
+			}
+			int buff = suffixArray[i];
+			suffixArray[i] = suffixArray[min];
+			suffixArray[min] = buff;
+		}
+
+        /* バブルソート */
+        /*for(int i=0;i<space.length;i++){
             for(int j=0;j<space.length-i-1;j++){
                 int ans = suffixCompare(j,j+1);
                 if(ans == 1){
@@ -58,14 +74,13 @@ public class Frequencer implements FrequencerInterface{
                     suffixArray[j] = buff;
                 }
             }
-        }
+        }*/
         printSuffixArray();
         //System.out.println("Dane");
     }
     
     
     private int targetCompare(int i, int start, int end) {
-        //if((i > mySpace.length) && (start < end))return -1;
         if((start == end) && (i <= mySpace.length)) return 0;
         if(mySpace[i]>myTarget[start]){
             return 1;
@@ -87,7 +102,8 @@ public class Frequencer implements FrequencerInterface{
         int n = mySpace.length - s;
         for(int k=0;k<n;k++) {
             if(mySpace[si+k]>mySpace[sj+k]) return 1;
-            if(mySpace[si+k]<mySpace[sj+k]) return -1; }
+            if(mySpace[si+k]<mySpace[sj+k]) return -1;
+        }
         if(si < sj) return 1;
         if(si > sj) return -1;
         return 0;
@@ -109,29 +125,6 @@ public class Frequencer implements FrequencerInterface{
         return suffixArray.length;
     }
 
-    
-    /*public int frequency() {
-     int count = 0;
-     if(myTarget == null){ return -1;}
-     else if(mySpace == null){return 0;}
-     else{
-     int targetLength = myTarget.length;
-     int spaceLength = mySpace.length;
-     
-     if(targetLength == 0){return -1;}
-     else if(spaceLength == 0){return 0;}
-     else{
-     for(int start = 0; start<spaceLength; start++) { // Is it OK?
-     boolean abort = false;
-     for(int i = 0; i<targetLength; i++) {
-     if(((start+i) >= spaceLength) ||(myTarget[i] != mySpace[start+i])){ abort = true; break; }
-     }
-					if(abort == false) { count++; }
-     }
-     }
-     }
-     return count;
-     }*/
     public int frequency() {
         if(targetReady == false) return -1;
         if(spaceReady == false) return 0;
@@ -140,28 +133,13 @@ public class Frequencer implements FrequencerInterface{
     
     // I know that here is a potential problem in the declaration.
     //public int subByteFrequency(int start, int length) {
-        // Not yet, but it is not currently used by anyone.
-     //   return -1;
+    // Not yet, but it is not currently used by anyone.
+    //   return -1;
     //}
     // I know that here is a potential problem in the declaration.
     public int subByteFrequency(int start, int end) {
         // Not yet, but it is not currently used by anyone.
-        /*int targetLength = myTarget.length;
-         int spaceLength = mySpace.length;
-         if(targetLength == 0)return -1;
-         if(!(start >= 0 && start <= end && end < targetLength)) return -1;
-         byte [] result = new byte[end - start];
-         for(int i = 0; i<end - start; i++) { result[i] = myTarget[start + i]; };
-         int count = 0;
-         int resultLength = result.length;
-         for(int head = 0; head<spaceLength; head++) { // Is it OK?
-         boolean abort = false;
-         for(int i = 0; i<resultLength; i++) {
-         if(result[i] != mySpace[head+i]) { abort = true; break; }
-         }
-         if(abort == false) { count++; }
-         }
-         return count;*/
+
         int first = subByteStartIndex(start,end);
         int last1 = subByteEndIndex(start, end);
         //inspection code
